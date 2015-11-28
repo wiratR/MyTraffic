14d415a0d747c52206b5d6cd1bb5e98a611b9c60
dorabon <wirateds@gmail.com>
2015-11-28 14:41:09 +0700

    test"

---
+++ app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
@@ -0,0 +1,24 @@
+package appewtc.masterung.mytraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity
+{
+    /// Explicit
+    private ListView trafficListView;   //decalre parameter inside class parameter
+    private Button aboutMEButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }   // Main Method
+
+}   // Main Class นี่คือคลาสหลัก นะคะ
