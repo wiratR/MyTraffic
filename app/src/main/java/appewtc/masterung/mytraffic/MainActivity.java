de5d3e3011834af2199087f4415fd6b2d09cdad7
dorabon <wirateds@gmail.com>
2015-11-29 10:03:36 +0700

    เทส สร้าง Method

--- app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
@@ -2,13 +2,13 @@ package appewtc.masterung.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
     /// Explicit
-    private ListView trafficListView;   //decalre parameter inside class parameter
+    private ListView trafficListView;   //declare parameter inside class parameter
     private Button aboutMEButton;
 
 
@@ -16,14 +16,29 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        // Bind Widget @คือดารผูก พารามิตอร์ กับ Widget
+        // Bind Widget @คือดารผูก พารามิตอร์ () กับ Widget
         bindWidget();
-        
 
+        //Button Controller
+        buttonController();
     }   // Main Method
 
-    private void bindWidget() {
+    private void buttonController() {
+        aboutMEButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            } //event
+        });
     }
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMEButton = (Button) findViewById(R.id.button);
+
+
+    }// bindWidget
 
 }   // Main Class นี่คือคลาสหลัก นะคะ
